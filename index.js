d25b8154d98ef74b0ec3e7c6abcfb08d21654730
dmtrung14 <trungdangminh14012004@gmail.com>
2022-07-11 00:19:11 +0700

    crime

--- index.js
+++ index.js
@@ -9,25 +9,7 @@ express()
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '816147459369763',
-      cookie     : true,
-      xfbml      : true,
-      version    : '14.0'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  
 
 FB.getLoginStatus(function(response) {
   statusChangeCallback(response);
