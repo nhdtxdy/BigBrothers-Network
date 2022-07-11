318a9f32b3a1eb627e0e8d708f7dffa01d3566e8
Duy Nguyen <fuy@nhdtxdy.localdomain>
2022-07-12 00:51:18 +0700

    Added index2.ejs as default view, switch to index if needed

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000 // 3000?
+require('dotenv').config();
+const devcert = require('devcert');
+const fs = require('fs');
 
-express()
+
+const https = require('https');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000 // 3000?
+console.log(process.env.APPID);
+const app = new express();
+const options = {
+  key: fs.readFileSync('localhost+1-key.pem'),
+  cert: fs.readFileSync('localhost+1.pem'),
+};
+const basketballPlayer = {
+  name: "James",
+  averagePointsPerGame: 20,
+  height: "6 feet, 2 inches",
+  position: "shooting guard"
+};
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.render('index2'))
+  // .get('/', (req, res) => res.render('index'))
+
+const server = https.createServer(options, app);
+server.listen(PORT, () => {
+  console.log(`Server is listening on https://localhost:${PORT}`);
+});
 
   
 
