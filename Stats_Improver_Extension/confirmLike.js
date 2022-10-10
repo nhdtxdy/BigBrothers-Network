04c7aece5c7d8bf137ebb79c3d3637bc8e753bd4
nhdtxdy <programmerplay@gmail.com>
2022-10-10 23:59:26 +0200

    Removed sensitive data

--- Stats_Improver_Extension/confirmLike.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.sendMessage({type : "GET_post"}, (res) => {
-    if (res.hidden) {
-        var content = document.getElementsByClassName('_1dwg ')[0];
-        content.remove();
-    }
-});
--- Stats_Improver_Extension/extensionCheck.js
+++
@@ -1,5 +0,0 @@
-var check = document.createElement('input');
-check.type = "hidden";
-check.id = "bigBrotherExtensionCheck";
-check.value = chrome.runtime.id;
-document.body.appendChild(check);
